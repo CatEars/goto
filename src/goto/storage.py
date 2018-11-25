fe39ef835117a2fc5658822d451f958301a600eb
Henke Adolfsson <catears13@gmail.com>
2018-11-25 20:34:23 +0100

    Add implementation for profiles

--- src/goto/storage.py
+++ src/goto/storage.py
@@ -51,15 +51,83 @@ def _retrieve_config(fname):
 
 def _read_config_file(fpath):
     '''Reads the content of a file and returns it.'''
-    with open(fpath, 'r') as f:
-        data = f.read()
-    return data
+    with open(fpath, 'r') as fhandle:
+        return toml.load(fhandle)
 
 
 def write_file(fpath, data):
     '''Writes the data to file.'''
-    with open(fpath, 'w') as f:
-        toml.dump(data, f)
+    with open(fpath, 'w') as fhandle:
+        toml.dump(data, fhandle)
+
+
+def _update_settings(data):
+    '''Updates the settings with new data.'''
+    fname = '_setting.toml'
+    home = get_config_home()
+    fpath = os.path.join(home, fname)
+    write_file(fpath, data)
+
+
+def _get_settings():
+    '''Returns the configuration settings.'''
+    fname = '_setting.toml'
+    home = get_config_home()
+    fpath = os.path.join(home, fname)
+    if not Path(fpath).exists():
+        default_values = {
+            'current_profile': 'default',
+            'profiles': ['default']
+        }
+        _update_settings(default_values)
+    return _read_config_file(fpath)
+
+def get_active_profile_name():
+    '''Returns the name of the active profile.'''
+    data = _get_settings()
+    return data.get('current_profile', 'default')
+
+
+def list_profiles():
+    '''Returns a list of all available profiles.'''
+    data = _get_settings()
+    return data.get('profiles', [])
+
+
+def add_profile(name):
+    '''Adds a profile.'''
+    data = _get_settings()
+    if name in data['profiles']:
+        msg = '{} is a profile that already exists'.format(name)
+        raise StorageException(msg)
+    if name.startswith('_'):
+        msg = '{} - you cannot start profiles with "_"'.format(name)
+        raise StorageException(msg)
+    if name == 'default':
+        msg = 'You cannot remove default settings'
+        raise StorageException(msg)
+
+    data['profiles'].append(name)
+    _update_settings(data)
+
+
+def remove_profile(name):
+    '''Removes a profile.'''
+    data = _get_settings()
+    if name not in data['profiles']:
+        msg = '{} - not a profile that exists'.format(name)
+        raise StorageException(msg)
+    data['profiles'].remove(name)
+    _update_settings(data)
+
+
+def set_current_profile(name):
+    '''Sets the current profile.'''
+    data = _get_settings()
+    if name.startswith('_'):
+        msg = '{} - you cannot start profiles with "_"'.format(name)
+        raise StorageException(msg)
+    data['current_profile'] = name
 
 
 def get_default_profile():
@@ -73,6 +141,7 @@ def update_default_profile(data):
 
 
 def update_named_profile(name, data):
+    '''Updates the values in a named profile.'''
     config_path = os.path.join(get_config_home(), '{}.toml'.format(name))
     write_file(config_path, data)
 
@@ -80,11 +149,13 @@ def update_named_profile(name, data):
 def get_named_profile(name, public_file=True):
     '''Returns the data of the specified profile.'''
     if public_file and name.startswith('_'):
-        raise StorageException('{} is an invalid name. Cannot start with "_"'.format(name))
+        msg = '{} is an invalid name. Cannot start with "_"'.format(name)
+        raise StorageException(msg)
+
     fpath = _retrieve_config('{}.toml'.format(name))
     try:
         data = _read_config_file(fpath)
-        return toml.loads(data)
+        return data
     except IOError:
         write_file(fpath, {})
         return {}
