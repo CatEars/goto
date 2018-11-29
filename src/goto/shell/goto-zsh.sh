c893dff3ccae9b666e2763ea55dc3c9490bbba41
Henke Adolfsson <henrik.adolfsson@briteback.com>
2018-11-29 16:54:30 +0100

    Update activation script

--- src/goto/shell/goto-zsh.sh
+++ src/goto/shell/goto-zsh.sh
@@ -1,3 +1,29 @@
 # This file is meant to be sourced
 # Entry point for zsh based shells
-echo "It is ZSH!"
+
+function _GotoHelperFunction() {
+
+}
+
+function goto() {
+    GT=_gotohelper
+    A=$1
+    B=$2
+    if [[ ! $A == -* ]] && [ -n "$A" ]; then
+        # We are dealing with a goto that wants to go somewhere
+        # The first argument is non-null and does not start with a dash!
+        target=$($GT --get $A)
+        if [ $? -ne 0 ]; then
+            echo "$target"
+        elif [ -d $target ]; then
+            cd $target
+        else
+            echo "'$target' seems to not be a directory..."
+        fi
+    else
+        # We are dealing with a command + argument
+        $GT $A $B
+    fi
+}
+
+compdef _GotoHelperFunction goto
