b66a888abbaa8e4901794a32435295c62160ccf6
Henke Adolfsson <catears13@gmail.com>
2018-12-11 20:10:34 +0100

    Add failing tests

---
+++ tests/test_cli.py
@@ -0,0 +1,49 @@
+'''Tests for cli implementation.'''
+
+
+import goto
+import pytest
+import shutil
+from goto import storage
+import test_util
+
+@test_util.custom_home
+def test_add():
+    '''Tests that add works with normal input.'''
+    assert False
+
+@test_util.custom_home
+def test_named_add():
+    '''Tests that add works with input like A_NAME:PATH/DIR.'''
+    assert False
+
+@test_util.custom_home
+def test_remove():
+    '''Tests that removing works.'''
+    assert False
+
+@test_util.custom_home
+def test_list():
+    '''Tests that listing works.'''
+    assert False
+
+
+@test_util.custom_home
+def test_profile():
+    '''Tests that changing profile works.'''
+    assert False
+
+
+@test_util.custom_home
+def test_list_profiles():
+    '''Tests that listing profiles work.'''
+    assert False
+
+@test_util.custom_home
+def test_remove_profile():
+    '''Tests that removing profiles work.'''
+    assert False
+
+
+
+
